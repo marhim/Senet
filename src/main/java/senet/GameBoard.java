ab6e26a4c26fb9420f0063e40a36b367a5f171ba
Marvin Himmelmeier <mail@marvinhi.de>
2020-11-09 14:02:46 +0100

    Cleaned up GameBoard

--- src/main/java/senet/GameBoard.java
+++ src/main/java/senet/GameBoard.java
@@ -6,28 +6,32 @@ public class GameBoard {
     private String[][] gb = new String[rows][cols];
     private int fieldcount = 30;
 
+    public GameBoard() {
+        generateGameBoard();
+    }
+
     /** Generates the game board with multiple for-loops.
      * TODO: Replace for-loops because they are unreadable, confusing and not maintainable.
      */
-    public void generateGameBoard(){
+    private void generateGameBoard(){
         for(int k = 0; k < rows; k++){
             if (k == 0){
                 for(int i = 0; i < cols; i++){
-                    if(i == 23 || i == 29){
+                    if(i == 22 || i == 28){
                         gb[k][i] = "-";
-                    }else if(i == 24) {
+                    }else if(i == 23) {
                         gb[k][i] = "S";
-                    }else if(i == 25 || i == 27) {
+                    }else if(i == 24 || i == 26) {
                         gb[k][i] = "e";
-                    }else if(i == 26) {
+                    }else if(i == 25) {
                         gb[k][i] = "n";
-                    }else if(i == 28) {
+                    }else if(i == 27) {
                         gb[k][i] = "t";
                     }else {
                         gb[k][i] = " ";
                     }
                 }
-            }else if (k == 1 || k == 10){
+            } else if (k == 1 || k == 10){
                 for(int i = 0; i < cols; i++){
                     if(i == 0 || i == 50){
                         gb[k][i] = "+";
@@ -35,173 +39,173 @@ public class GameBoard {
                         gb[k][i] = "-";
                     }
                 }
-            }else if (k == 2){
+            } else if (k == 2){
                 for(int i = 0; i < cols; i++){
                     if (i == 0){
                         for(int j = i; j < cols; j += 5){
                             gb[k][j] = "|";
                         }
-                    }else if (i == 1 || i == 4){
+                    } else if (i == 1 || i == 4){
                         for(int j = i; j < cols; j += 5){
                             if (j == 21 || j == 24){
                                 gb[k][j] = "-";
-                            }else {
+                            } else {
                                 gb[k][j] = " ";
                             }
                         }
-                    }else if (i == 2){
+                    } else if (i == 2){
                         for(int j = i; j < cols; j += 5){
                             if (j == 47){
                                 gb[k][j] = "1";
-                            }else {
+                            } else {
                                 gb[k][j] = "0";
                             }
                         }
-                    }else if (i == 3){
+                    } else if (i == 3){
                         gb[k][i] = "1";
-                    }else if (i == 8){
+                    } else if (i == 8){
                         gb[k][i] = "2";
-                    }else if (i == 13){
+                    } else if (i == 13){
                         gb[k][i] = "3";
-                    }else if (i == 18){
+                    } else if (i == 18){
                         gb[k][i] = "4";
-                    }else if (i == 23){
+                    } else if (i == 23){
                         gb[k][i] = "5";
-                    }else if (i == 28){
+                    } else if (i == 28){
                         gb[k][i] = "6";
-                    }else if (i == 33){
+                    } else if (i == 33){
                         gb[k][i] = "7";
-                    }else if (i == 38){
+                    } else if (i == 38){
                         gb[k][i] = "8";
-                    }else if (i == 43){
+                    } else if (i == 43){
                         gb[k][i] = "9";
-                    }else if (i == 48){
+                    } else if (i == 48){
                         gb[k][i] = "0";
                     }
                 }
-            }else if (k == 3 || k == 6 || k == 9){
+            } else if (k == 3 || k == 6 || k == 9){
                 for(int i = 0; i < cols; i++){
                     if (i == 0){
                         for(int j = i; j < cols; j += 5){
                             gb[k][j] = "|";
                         }
-                    }else if (i == 1 || i == 4){
+                    } else if (i == 1 || i == 4){
                         for(int j = i; j < cols; j += 5){
                             if (k == 9 && j == 31){
                                 gb[k][j] = "/";
-                            }else if (k == 9 && j == 34){
+                            } else if (k == 9 && j == 34){
                                 gb[k][j] = "\\";
-                            }else{
+                            } else{
                                 gb[k][j] = ":";
                             }
                         }
-                    }else if (i == 2 || i == 3){
+                    } else if (i == 2 || i == 3){
                         for(int j = i; j < cols; j += 5){
                             gb[k][j] = " ";
                         }
                     }
                 }
-            }else if (k == 4 || k == 7){
+            } else if (k == 4 || k == 7){
                 for(int i = 0; i < cols; i++){
                     if (i%5 == 0){
                         gb[k][i] = "+";
-                    }else {
+                    } else {
                         gb[k][i] = "-";
                     }
                 }
-            }else if (k == 5){
+            } else if (k == 5){
                 for(int i = 0; i < cols; i++){
                     if (i == 0){
                         for(int j = i; j < cols; j += 5){
                             gb[5][j] = "|";
                         }
-                    }else if (i == 1 || i == 4){
+                    } else if (i == 1 || i == 4){
                         for(int j = i; j < cols; j += 5){
                             if (j == 26){
                                 gb[k][j] = "[";
-                            }else if (j == 29){
+                            } else if (j == 29){
                                 gb[k][j] = "]";
-                            }else {
+                            } else {
                                 gb[k][j] = " ";
                             }
                         }
-                    }else if (i == 2){
+                    } else if (i == 2){
                         for(int j = i; j < cols; j += 5){
                             if (j == 2){
                                 gb[k][j] = "2";
-                            }else {
+                            } else {
                                 gb[k][j] = "1";
                             }
                         }
-                    }else if (i == 3){
+                    } else if (i == 3){
                         gb[k][i] = "0";
-                    }else if (i == 8){
+                    } else if (i == 8){
                         gb[k][i] = "9";
-                    }else if (i == 13){
+                    } else if (i == 13){
                         gb[k][i] = "8";
-                    }else if (i == 18){
+                    } else if (i == 18){
                         gb[k][i] = "7";
-                    }else if (i == 23){
+                    } else if (i == 23){
                         gb[k][i] = "6";
-                    }else if (i == 28){
+                    } else if (i == 28){
                         gb[k][i] = "5";
-                    }else if (i == 33){
+                    } else if (i == 33){
                         gb[k][i] = "4";
-                    }else if (i == 38){
+                    } else if (i == 38){
                         gb[k][i] = "3";
-                    }else if (i == 43){
+                    } else if (i == 43){
                         gb[k][i] = "2";
-                    }else if (i == 48){
+                    } else if (i == 48){
                         gb[k][i] = "1";
                     }
                 }
-            }else if (k == 8){
+            } else if (k == 8){
                 for(int i = 0; i < cols; i++){
                     if (i == 0){
                         for(int j = i; j < cols; j += 5){
                             gb[k][j] = "|";
                         }
-                    }else if (i == 1 || i == 4){
+                    } else if (i == 1 || i == 4){
                         for(int j = i; j < cols; j += 5){
                             if (j == 26 || j == 36 || j == 41 || j == 46){
                                 gb[k][j] = "(";
-                            }else if (j == 29 || j == 39 || j == 44 || j == 49){
+                            } else if (j == 29 || j == 39 || j == 44 || j == 49){
                                 gb[k][j] = ")";
-                            }else if (j == 31){
+                            } else if (j == 31){
                                 gb[k][j] = "\\";
-                            }else if (j == 34){
+                            } else if (j == 34){
                                 gb[k][j] = "/";
-                            }else {
+                            } else {
                                 gb[k][j] = " ";
                             }
                         }
-                    }else if (i == 2){
+                    } else if (i == 2){
                         for(int j = i; j < cols; j += 5){
                             if (j == 47){
                                 gb[k][j] = "3";
-                            }else {
+                            } else {
                                 gb[k][j] = "2";
                             }
                         }
-                    }else if (i == 3){
+                    } else if (i == 3){
                         gb[k][i] = "1";
-                    }else if (i == 8){
+                    } else if (i == 8){
                         gb[k][i] = "2";
-                    }else if (i == 13){
+                    } else if (i == 13){
                         gb[k][i] = "3";
-                    }else if (i == 18){
+                    } else if (i == 18){
                         gb[k][i] = "4";
-                    }else if (i == 23){
+                    } else if (i == 23){
                         gb[k][i] = "5";
-                    }else if (i == 28){
+                    } else if (i == 28){
                         gb[k][i] = "6";
-                    }else if (i == 33){
+                    } else if (i == 33){
                         gb[k][i] = "7";
-                    }else if (i == 38){
+                    } else if (i == 38){
                         gb[k][i] = "8";
-                    }else if (i == 43){
+                    } else if (i == 43){
                         gb[k][i] = "9";
-                    }else if (i == 48){
+                    } else if (i == 48){
                         gb[k][i] = "0";
                     }
                 }
